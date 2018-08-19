bb3bf6b6026fd205c3b68ad8c7b4b636b002723c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-08-19 19:59:46 +0200

    Adding images

--- app/twentyone/utils/deck/Card.scala
+++ app/twentyone/utils/deck/Card.scala
@@ -21,6 +21,31 @@ case class Card(suit: CardSuit.Value, number: CardNumber.Value) {
     case CardNumber.Queen => 2
     case CardNumber.King => 3
   }
+
+  def imgFileName: String = {
+    val suitName = suit match {
+      case CardSuit.Spade => "spades"
+      case CardSuit.Club => "clubs"
+      case CardSuit.Heart => "hearts"
+      case CardSuit.Diamond => "diamonds"
+    }
+    val numberName = number match {
+      case CardNumber.Ace => "ace"
+      case CardNumber.n2 => 2
+      case CardNumber.n3 => 3
+      case CardNumber.n4 => 4
+      case CardNumber.n5 => 5
+      case CardNumber.n6 => 6
+      case CardNumber.n7 => 7
+      case CardNumber.n8 => 8
+      case CardNumber.n9 => 9
+      case CardNumber.n10 => 10
+      case CardNumber.Jack => "jack"
+      case CardNumber.Queen => "queen"
+      case CardNumber.King => "king"
+    }
+    s"${numberName}_of_$suitName.svg"
+  }
 }
 
 object Card {
